66a58ab054e9a1925384a09ca311216884ce76a9
unknown <raj.vatnani@RAJKUMAR-VATNAN.diasparkindia.com>
2016-02-19 12:55:48 +0530

    no message

---
+++ WebClient/Controller/Controller.js
@@ -0,0 +1,61 @@
+﻿
+app.controller('mycontroller', function ($scope, mynodeservice) {
+    
+   
+    $scope.Contact = {       
+        FirstName: "",
+        LastName: "",
+        Address: "",
+        City: "",
+        State: "",
+        Pincode:"",
+        Email:""
+    }
+
+    loaddata();
+   
+    function loaddata() {
+        debugger;
+        var promise = mynodeservice.get();
+
+        promise.then(function (resp) {
+            $scope.ContactList = resp.data;
+            $scope.message = "Call is Successful ";
+        }, function (err) {
+            $scope.message = "Error in Call" + err.status
+        });
+    };
+   
+    $scope.save = function () {
+        debugger;
+        var promisePost = mynodeservice.post($scope.Contact);
+        promisePost.then(function (resp) {            
+            $scope.message = "Call is Successful";            
+            loaddata();
+        }, function (err) {
+            $scope.message = "Error in Call" + err.status
+        });
+    };
+
+    $scope.clear = function () {
+        $scope.Contact.FirstName = "";
+        $scope.Contact.LastName = "";
+        $scope.Contact.Address = "";
+        $scope.Contact.City = "";
+        $scope.Contact.State = "";
+        $scope.Contact.Pincode = "";
+        $scope.Contact.Email = "";
+    };
+
+    $scope.delete = function (cnt) {
+        debugger;       
+        var promiseDelete = mynodeservice.put(cnt);
+
+        promiseDelete.then(function (resp) {
+            $scope.message = "Call is Successful";
+            loaddata();
+        }, function (err) {
+            $scope.message = "Error in Call" + err.status
+        });
+    }
+});
---
+++ WebClient/Controller/service.js
@@ -0,0 +1,17 @@
+﻿app.service('mynodeservice', function ($http) {
+    this.get = function () {
+        var res = $http.get("http://localhost:8080/ContactList/api/Contact");
+        return res;
+    }
+
+    this.post = function (cnt) {      
+        var res = $http.post("http://localhost:8080/ContactList/api/Contact", cnt);
+        return res;
+    }
+
+    this.put = function (cnt) {
+        debugger;
+        var res = $http.put("http://localhost:8080/ContactList/api/Contact/", cnt);
+        return res;
+    }
+});
